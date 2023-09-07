2598c88aa5fda0f294814439e0fb3dc9c15b627f
SyedJunaid <syedjunaid2692@gmail.com>
2023-09-07 23:02:58 +0500

    - Fix Issue of ON-Hold Case

--- src/env.js
+++ src/env.js
@@ -3,7 +3,7 @@
 export const env = {
   REACT_APP_TITLE: "Pharmacy",
   REACT_APP_VERSION: "v1.0",
-  REACT_APP_BASE_URL: "http://192.168.18.10:3000/api",
+  REACT_APP_BASE_URL: "http://146.190.174.27:3000/api",
   
 };
 
